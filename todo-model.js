51c4310d04fd1f0a781976da40a23e07183dfc89
jamshid <j6054230@gmail.com>
2026-05-15 17:17:05 +0500

    feat(model): addTodo va removeTodo metodlari

--- todo-model.js
+++ todo-model.js
@@ -5,3 +5,8 @@ class Todo {
     this.done = false; 
   }
 }
+class TodoList {
+  constructor() { this.todos = []; }
+  addTodo(text) { this.todos.push(new Todo(text)); }
+  removeTodo(id) { this.todos = this.todos.filter(t => t.id !== id); }
+}
